4530534ffb32570a21af45af7fb1ba44087485a1
KELVIN-MUGI <kelvinmugi01@gmail.com>
2024-08-02 23:57:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -37,7 +37,7 @@ const FOOTERNAME = '*Powered by TalkDrove*'; //Footer text
 //----------------OWNER NUMBER------------------------------//
 //////////////////////////////////////////////////////////////////
 
-global.owner = process.env.OWNER_NUMBER  || '923072380380' ;  //put your number without +
+global.owner = process.env.OWNER_NUMBER  || '254114537515' ;  //put your number without +
 
 /////////////////////////////////////////////////////////////////
 //-------------------------------------------------------//
@@ -59,7 +59,7 @@ module.exports = { //exporting modules to use in plugins
 //----------------Session ID------------------------------//
 //////////////////////////////////////////////////////////////////
 
-SESSION_ID: process.env.SESSION_ID || 'Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOEVzVE10RGExcm0xbHVUUGt2M0trRklYWjd0WHR6TWlsQTNXajJjWTcwaz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVTZnNDBnMDFPM2s5OUQvd3d0bk80dHdST2grUzRGbE5YM1BxM0xCb29CWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ3Sk83ZExhZzNOWkVOKy91ekVjU0hjalovQnFxeGVkTDZWblhYZXhwR2tVPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJYV01CL0RINUdGZG5KNG85NndJSXdkRm1nL1ZJY0lFbUx5am9zUWlnU2dNPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjJKZkUxeC9zMmoxSDJRdWV1Mm9ZZXVleVJZN2c3Wk1iV0dTaDlXUk05RjQ9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InZaeUF2bmhzaXlGQzlQSWhXOGZXU3M1MURPZEd4VUtNeFFlUVlhRld0eGs9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibU01MnYwRTdXYjRpblRScGhlNVJwelRpSFdmQ3ZPTGgwblRaUnZ4aEYxQT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMVFIV3RiQmNTMlAzak00anRGSURTaUZ3OTlXTGJTSlRYa3pLSTgxSVEwQT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjhkTkdlS1lJSWtIeHZVSWxNc2s5cXlDS3lVSUdFbHZac0FoRWYwRnFneTVpMHRyWWJVSmNpeGFVOWVEVkZaWXNzN2pFcnVRditka2JFaDJiQXZWU2l3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NDQsImFkdlNlY3JldEtleSI6IldFNEl5ZlpJL2FoVHlVRml4STE2cEJrSDBrOTNyWGJ6U1N5RFFjT21iaTQ9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6IlBnWjRZUW05U2YteHFCemtlQzVlenciLCJwaG9uZUlkIjoiZWE4YzdhMjYtMWE5Yi00MjI2LWI5ODYtZDBiNzgxMjA1MGQzIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik9BTjk5OUZYbGJZTUhNUjQwbzRhd3BkUFZvQT0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI1akI0aXZDVDk5ZnNNdU82ODRocStid3BQOUE9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiV1BOV1E1TVgiLCJtZSI6eyJpZCI6IjI1NDExNDUzNzUxNTo2MUBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDSlRwNzFBUXZmMnp0UVlZQVNBQUtBQT0iLCJhY2NvdW50U2lnbmF0dXJlS2V5Ijoia291Vk9qSDR3N3AzZlJYVWVqUkgyVG5WVENYVFlxTGhtVHRBdTNqV0ZUbz0iLCJhY2NvdW50U2lnbmF0dXJlIjoic2lycXNkZC9MY0xsYzl5MlA5VDNYNUlWSjdtMHRnQlZ6ZG0rOUQxS01LZkxDSis0eGpxUld1S2JscG5jdVZwRlp2c0llSFBodG1jbWd1MXp0UHJpRHc9PSIsImRldmljZVNpZ25hdHVyZSI6Im1HbzRMS1R0WjVRa0trVWo1Q2x5SUdYTFhCOE9lc3BqRWxOZHJaSDE2V2x1aTJ4MDVCLytoQm5pRGNyWnlZWDZiUEoyMUdaMTB6LzlYWUJ6bzUvN2lRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjU0MTE0NTM3NTE1OjYxQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlpLTGxUb3grTU82ZDMwVjFIbzBSOWs1MVV3bDAyS2k0Wms3UUx0NDFoVTYifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MjI2MTM0NTB9', //Paste your session ID here
+SESSION_ID: process.env.SESSION_ID || 'Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibU04TWtIWWJGMElRdlZqcUxLRHh2Y3N5ZjJVU3U2emRpcm8vSFcxQklVMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSHM4a1BieDRIZ29jd25aZDA5TkRyenNHd1M5VHFCN2NRWElGR3ZMazhXQT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJNSzZDWlo2c1dMYjlvUFVaYVAwb2JwNzV6UU9odnlBYW9NNGdhUWRCakY4PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJvMnpNd29BbnNCVEl3cHNiL1FYemM1dU5Za2xRMFRuZitGRWFYL1ZTS1U4PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImNLU1NwY3VCVUkyTm1LMDB4UWJVT2IxR3NFbDFzNVNmR3JMTmx2Y2dBMm89In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImkzeC8rZEF2elNYWDhObStVSTZkVld0dGsvL200OStXRFZncGRhczZjd2M9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoid0NKREZKZllNM09uK050Q3ZXaDVCSEdRWmJGVVNtQjVpK3loRWJXcFZuQT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOVYxNnBBTHBiVHRwMHpOWUZRRlUycmhtZVgrZGJ3VkpNRTRXRGdoN0pRND0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Img4cnpYM2pTRFFRR0FGR2lVMGJaRHdHdmN1NDU5R245d3BESFdCYVlNcDJiZ0V3ZFJFVFhFUHRpZk0xMldGNWE5ZEJaY0w0ZzhtdEQrY3J2TjhhVUJRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjQ5LCJhZHZTZWNyZXRLZXkiOiJHckhHd1V2cnFQQkpwMlQzQUplYldVSGNPS09SVzRoZHRLT0ZaMW1BaUs4PSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzIsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMiwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJlNkZzTnhhclQ0U09QWThmc3RBVm1nIiwicGhvbmVJZCI6ImM0NDRiMGUxLTA1YjUtNDRjNy04NjM0LTQzMjEzNGUwNjQwYSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJOMktRMzZNSlFPVVkrYm5JQjBOays1YnlTZzQ9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVDNHdGl1cWY5MjM3L0VldWNpVmd0RWlLZlFnPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IjJDV1BaQjRQIiwibWUiOnsiaWQiOiIyNTQxMTQ1Mzc1MTU6NjNAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0pUcDcxQVFtNDIxdFFZWUF5QUFLQUE9IiwiYWNjb3VudFNpZ25hdHVyZUtleSI6ImtvdVZPakg0dzdwM2ZSWFVlalJIMlRuVlRDWFRZcUxobVR0QXUzaldGVG89IiwiYWNjb3VudFNpZ25hdHVyZSI6IlhMa2RqbVU1NEJXVjluZzQ0MjZTWGZ3VjZhdFlkcmJZMHVNS1dHVjVCTHdUTU5Qcm4rWGE5QjZPK3krSGE2VHBhYnRQM005OGJ4U01jbHhFQVBsV0NBPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJQaXBlM05UbTgxSWdrRjNCU3A4SStEdmdZL3ZmOW9FNXRCK0NVV05obGtQSlZsTFNwbEpDS3ZKMWUrWmRSa3FPajg3dks1VWl0a0tSYmxiSWc1TXNBZz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjI1NDExNDUzNzUxNTo2M0BzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJaS0xsVG94K01PNmQzMFYxSG8wUjlrNTFVd2wwMktpNFprN1FMdDQxaFU2In19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIyNjMxODQ4fQ==', //Paste your session ID here
 
 /////////////////////////////////////////////////////////////////
 //-------------------------------------------------------//
@@ -86,10 +86,10 @@ LOGO: process.env.LOGO || `https://raw.githubusercontent.com/HyHamza/HyHamza/mai
 LANG:  process.env.LANG || 'EN' , //URDU OR EN
 //////////////////////////////////////////////////////////
 
-PREFIX: process.env.PREFIX || '.' , //PREFIX, LEAVE IT DEFAULT OTHERWISE BUTTON WILL NOT WORK
+PREFIX: process.env.PREFIX || '¶' , //PREFIX, LEAVE IT DEFAULT OTHERWISE BUTTON WILL NOT WORK
 //////////////////////////////////////////////////////////
 
-WORK_TYPE: process.env.WORK_TYPE || 'public' , //public or private
+WORK_TYPE: process.env.WORK_TYPE || 'private' , //public or private
 //////////////////////////////////////////////////////////
 
 travaSend: process.env.TRAVA_SEND || '25' ,   //BUGS
@@ -98,13 +98,13 @@ travaSend: process.env.TRAVA_SEND || '25' ,   //BUGS
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' , //button or nonbutton
 //////////////////////////////////////////////////////////
 
-MODERATORS : process.env.MODERATORS === undefined ? "923072380380" : process.env.MODERATORS ,    
+MODERATORS : process.env.MODERATORS === undefined ? "254114537515" : process.env.MODERATORS ,    
 //////////////////////////////////////////////////////////
 
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE, //Maximum size that bot can download a file   
 //////////////////////////////////////////////////////////
 
-OWNER_NAME: process.env.OWNER_NAME || 'Hamza' , //Your name
+OWNER_NAME: process.env.OWNER_NAME || 'VANISH' , //Your name
 //////////////////////////////////////////////////////////
 
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9' : process.env.POSTGRESQL_URL,
